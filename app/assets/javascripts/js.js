2a1adb69c7ab69577e49384f435dcfdf7df26bfc
Mike Weber <mike.weber@govdelivery.com>
2016-03-20 21:49:31 -0400

    remove before change warning

--- app/assets/javascripts/js.js
+++ app/assets/javascripts/js.js
@@ -12,16 +12,4 @@
   function isBlank(val) {
     return typeof val === 'undefined' || val === null || val === ''
   }
-
-  var msg = "You have unsaved changes."
-  $(window).bind('beforeunload', function() {
-    if (hasPendingChange()) return msg
-  })
-  $(document).on('page:before-change', function() {
-    if (hasPendingChange()) return confirm(msg)
-  })
-
-  function hasPendingChange() {
-    return $('.track_changes.changed').length > 0
-  }
 })(jQuery)
